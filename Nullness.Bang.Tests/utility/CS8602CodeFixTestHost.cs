4566c069d88157b8f67a63b9925d3a94203cfce3
Joost <me@joost.rocks>
2023-04-22 17:39:09 +0200

    fix test

--- Nullness.Bang.Tests/utility/CS8602CodeFixTestHost.cs
+++ Nullness.Bang.Tests/utility/CS8602CodeFixTestHost.cs
@@ -1,19 +1,12 @@
 ﻿using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp.Testing;
-using Microsoft.CodeAnalysis.Testing;
 using Microsoft.CodeAnalysis.Testing.Verifiers;
 
 namespace Nullness.Bang.Tests.utility
 {
     public class CS8602CodeFixTestHost : CSharpCodeFixTest<DummyAnalyzer, EmptyCodeFixProvider, MSTestVerifier>
     {
-        public CS8602CodeFixTestHost()
-        {
-            //ReferenceAssemblies = CodeAnalyzerHelper.CurrentXunitV2;
-
-            // xunit diagnostics are reported in both normal and generated code
-            TestBehaviors |= TestBehaviors.SkipGeneratedCodeCheck;
-        }
+      
 
         protected override IEnumerable<CodeFixProvider> GetCodeFixProviders()
         {
